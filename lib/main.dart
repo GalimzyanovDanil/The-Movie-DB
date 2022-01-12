f36a6d923e965300595e5f0c6ca24a8fe44f82a6
Danil Galimzyanov <galimzyanov.ing@yandex.ru>
2022-01-12 09:57:54 +0300

    Init project. Sowe problem ignore.

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:the_movie_db/Theme/app_colors.dart';
+import 'package:the_movie_db/widgets/auth/auth_widget.dart';
+import 'package:the_movie_db/widgets/main_screen/main_screen_widget.dart';
+import 'package:the_movie_db/widgets/movie_details/movie_details_widget.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: AppColors.mainDarkBlue,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: AppColors.mainDarkBlue,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+        ),
+      ),
+      routes: {
+        '/auth': (context) => AuthWidget(),
+        '/main_screen': (context) => MainScreenWidget(),
+        '/main_screen/movie_details': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments;
+          if (arguments is int) {
+            return MovieDetailsWidget(movieId: arguments);
+          } else {
+            return MovieDetailsWidget(movieId: 0);
+          }
+        },
+      },
+      initialRoute: '/auth',
+    );
+  }
+}
---
+++ lib/widgets/main_screen/main_screen_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:the_movie_db/widgets/movie_list/movie_list_widget.dart';
+
+
+class MainScreenWidget extends StatefulWidget {
+ const  MainScreenWidget({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenWidgetState createState() => _MainScreenWidgetState();
+}
+
+class _MainScreenWidgetState extends State<MainScreenWidget> {
+  int _selectedTab = 0;
+
+  void onSelectTab(int index) {
+    if (_selectedTab == index) return;
+    setState(() {
+      _selectedTab = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const  Text('TMDB'),
+      ),
+      body: IndexedStack(
+        index: _selectedTab,
+        children: [
+          const Text(
+            'Новости',
+          ),
+          MovieListWidget(),
+          const Text(
+            'Сериалы',
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedTab,
+        items:const  [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Новости',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie_filter),
+            label: 'Фильмы',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.tv),
+            label: 'Сериалы',
+          ),
+        ],
+        onTap: onSelectTab,
+      ),
+    );
+  }
+}
